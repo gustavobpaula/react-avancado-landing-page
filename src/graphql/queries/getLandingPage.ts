f50eead61730d1499de26e6750f1a56830bd27f9
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2021-03-10 21:51:35 -0300

    feat: 🎸 add dynamic content

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,146 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
-        alternativeText
+  # Write your query or mutation here
+  fragment imageData on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...imageData
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
         url
       }
+      image {
+        ...imageData
+      }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      image {
+        ...imageData
+      }
+    }
+  }
+
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      title
+      techIcons {
+        title
+        icon {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionConcepts on LandingPage {
+    sectionConcepts {
+      title
+      concepts {
+        title
+      }
+    }
+  }
+
+  fragment sectionModules on LandingPage {
+    sectionModules {
+      title
+      modules {
+        title
+        subtitle
+        description
+      }
+    }
+  }
+
+  fragment sectionAgenda on LandingPage {
+    sectionAgenda {
+      title
+      description
+    }
+  }
+
+  fragment pricingBox on LandingPage {
+    pricingBox {
+      totalPrice
+      numberInstallments
+      priceInstallments
+      benefits
+      button {
+        label
+        url
+      }
+    }
+  }
+
+  fragment sectionAboutUs on LandingPage {
+    sectionAboutUs {
+      title
+      authors {
+        photo {
+          ...imageData
+        }
+        name
+        role
+        socialLinks {
+          title
+          url
+        }
+        description
+      }
+    }
+  }
+
+  fragment sectionReviews on LandingPage {
+    sectionReviews {
+      title
+      reviews {
+        name
+        text
+        photo {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionFaq on LandingPage {
+    sectionFaq {
+      title
+      questions {
+        question
+        answer
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+      ...sectionTech
+      ...sectionConcepts
+      ...sectionModules
+      ...sectionAgenda
+      ...pricingBox
+      ...sectionAboutUs
+      ...sectionReviews
+      ...sectionFaq
     }
   }
 `
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,123 @@
-export type LogoProps = {
+export type TechIconProps = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type ImageProps = {
   alternativeText: string
   url: string
 }
 
+export type LogoProps = ImageProps
+
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: ImageProps
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: ImageProps
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIconProps[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: Array<{
+    title: string
+  }>
+}
+
+export type ModulesProps = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: ModulesProps[]
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallments: number
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
+export type SocialLinksProps = {
+  title
+  url
+}
+
+export type AuthorProps = {
+  photo: ImageProps
+  name: string
+  role: string
+  socialLinks: SocialLinksProps[]
+  description: string
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: AuthorProps[]
+}
+
+export type ReviewProps = {
+  name: string
+  text: string
+  photo: {
+    url: string
+  }
+}
+
+export type SectionReviewsProps = {
+  title: string
+  reviews: ReviewProps[]
+}
+
+export type QuestionProps = {
+  question: string
+  answer: string
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: QuestionProps[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
